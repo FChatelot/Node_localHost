ef0f0a4a9508dd83749dbb0e5f0e35d9e9427ee4
FChatelot <florianchatelot@gmail.com>
2023-12-01 17:21:30 +0100

    .

--- node_hosting/express_test/app.js
+++ node_hosting/express_test/app.js
@@ -5,15 +5,18 @@ const PORT = 8080;
 //j'utilise ejs pour render mon application
 app.set("view engine","ejs");
 //routes
-app.get("/",(req,res)=> res.render("pages/index"))
-app.get
+app.get("/",(req,res)=> res.render("pages/index"));
+app.get("/logIn",(req,res)=> res.render("pages/user/logIn"));
+app.get("/logOut",(req,res)=> res.render("pages/user/logOut"));
+app.get("/signIn",(req,res)=> res.render("pages/user/signIn"));
+app.get("/userProfil",(req,res)=> res.render("pages/user/userProfil"));
 
 app.listen(PORT,()=>{
-    console.log ('Le serveur est lancé')
-})
+    console.log ('Le serveur est lancé');
+});
 
 //en cas d'erreur au lancement
 process.on("unhandledRejection",err => {
-    console.log(`Une erreur est survenue: ${err.message}`)
-    server.close(()=> process.exit(1))
-})
+    console.log(`Une erreur est survenue: ${err.message}`);
+    server.close(()=> process.exit(1));
+});
