2203bc22c5deaa08d8c8b9c39e4a9090cfcb2c3f
FChatelot <florianchatelot@gmail.com>
2023-12-04 16:47:40 +0100

    v0.0.2

--- node_hosting/express_test/app.js
+++ node_hosting/express_test/app.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const path = require('path');
+
 
 //j'utilise ejs pour render mon application
 app.set("view engine","ejs");
+
+/* j'utilise express.static pour définir un dossier statique pour mes différents
+ assets (css/img).*/
+ 
+app.use(express.static(path.join(__dirname, 'public')));
 //routes
 app.get("/",(req,res)=> res.render("pages/index"));
 app.get("/logIn",(req,res)=> res.render("pages/user/logIn"));
 app.get("/logOut",(req,res)=> res.render("pages/user/logOut"));
 app.get("/signIn",(req,res)=> res.render("pages/user/signIn"));
 app.get("/userProfil",(req,res)=> res.render("pages/user/userProfil"));
+
 
 app.listen(PORT,()=>{
     console.log ('Le serveur est lancé');
